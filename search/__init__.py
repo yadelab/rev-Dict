bfb302034d9333ef2057eaf69dc870d28b4fe80a
Christopher Qian <cqian19@hotmail.com>
2015-10-03 18:50:55 -0700

    Initial commit

---
+++ search/__init__.py
@@ -0,0 +1,91 @@
+import requests, re
+
+API_KEY = 'eb4e57bb2c34032da68dfeb3a0578b68'
+URL_MASK = 'http://words.bighugelabs.com/api/2/{key}/{word}/'
+ABBREVIATIONS = {
+	'Synonym': 'syn',
+	'Antonym': 'ant',
+}
+
+def _lookup_word(word, relationship, begins_with, ends_with):
+	"""Looks up a word in the Big Huge Thesaurus and returns
+	similar words that fit the parameters.
+
+	:param word: word to look up
+	:param relationship: relationship to entered word ('Synonym' or 'Antonym')
+	:param begins_with: letters that the desired word starts with
+	:param ends_with: letters that the desired word ends with
+
+	:return: a list of similar words that match the requirements
+	"""
+	url = URL_MASK.format(key= API_KEY, word = word)
+	r = requests.get(url)
+	text = r.text.split('\n')
+	
+	relationship = ABBREVIATIONS[relationship]
+	word_re = re.compile('^' + begins_with + '(.+)?' + ends_with + '$')
+
+	found_words = []
+	for group in text:
+		s = group.split('|')
+		if len(s) > 1:
+			article, rel, synonym = s
+			if rel == relationship:
+				if begins_with or ends_with:
+					if word_re.search(synonym):
+						found_words.append(synonym)
+				else:
+					found_words.append(synonym)
+
+	return found_words
+
+def _sort_by_frequency (all_words):
+	frequency_list = {}
+	for group in all_words:
+		for word in group:
+			if not word in frequency_list:
+				frequency_list[word] = 1
+			else:
+				frequency_list[word] += 1
+
+	return frequency_list
+def _get_highest_frequency(frequency_list):
+	"""Returns the highest frequency value in a list sorted by word frequency.
+
+		:param frequency_list: a list of words and their corresponding frequency
+
+		:return: the highest frequency value in the frequency_list
+	"""
+	highest = 0
+	for frequency in frequency_list.values():
+		if frequency > highest:
+			highest = frequency
+
+	return highest
+
+def parse_search_entry(entry, relationship, begins_with = '', ends_with = ''):
+	"""Takes a search entry and returns the words with the highest frequency.
+
+	:param word: word to look up
+	:param relationship: relationship to entered word ('Synonym' or 'Antonym')
+	:param begins_with: letters that the desired word starts with
+	:param ends_with: letters that the desired word ends with
+
+	"""
+	entry = entry.strip()
+	words = entry.split(' ')
+	all_related_words = []
+	for word in words:
+		related_words = _lookup_word(word, relationship, begins_with, ends_with)
+		all_related_words.append(related_words)
+
+	words_by_frequency = _sort_by_frequency(all_related_words)
+	highest_frequency = _get_highest_frequency(words_by_frequency)
+
+	best_related_words = [word for word, freq in words_by_frequency.items() if freq == highest_frequency]
+
+	if len(best_related_words) < 10:
+		return best_related_words
+	else:
+		return best_related_words[0:10]
+
---
+++ server.py
@@ -0,0 +1,44 @@
+import sqlite3
+import time
+from flask import Flask, request, g, render_template, redirect
+
+app = Flask(__name__)
+DATABASE = 'hakks.db'
+
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DATABASE)
+	return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
+
+def db_read_hakks():
+	cur = get_db().cursor()
+	cur.execute("SELECT * FROM hakks")
+	return cur.fetchall()
+
+def db_add_hakk(name, hakk):
+	cur = get_db().cursor()
+	t = str(time.time())
+	hakk_info = (name, t, hakk)
+	cur.execute("INSERT INTO hakks VALUES (?, ?, ?)", hakk_info)
+	get_db().commit()
+
+@app.route("/")
+def hello():
+	hakks = db_read_hakks()
+	return render_template('index.html', hakks = hakks)
+
+@app.route("/api/hakk", methods=["POST"])
+def receive_hakk():
+	print(request.form)
+	db_add_hakk(request.form['name'], request.form['hakk'])
+	return redirect("/")
+
+if __name__ == "__main__":
+	app.run()
