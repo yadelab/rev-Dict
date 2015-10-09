03f90b686c92b0d239eb914abcf25b944121e38a
Christopher Qian <cqian19@hotmail.com>
2015-10-08 23:32:13 -0700

    Fixed issue with entry being empty Added support for words with no results Cleaned up home.html Removed unnecessary database functions/files

--- server.py
+++ server.py
@@ -17,20 +17,8 @@ def close_connection(exception):
 	if db is not None:
 		db.close()
 
-def db_read_hakks():
-	cur = get_db().cursor()
-	cur.execute("SELECT * FROM hakks")
-	return cur.fetchall()
-
-def db_add_hakk(search, results):
-	cur = get_db().cursor()
-	hakk_info = (search, results)
-	cur.execute("INSERT INTO hakks VALUES (?, ?)", hakk_info)
-	get_db().commit()
-
 @app.route("/")
 def hello():
-	hakks = db_read_hakks()
 	return render_template('home.html')
 
 @app.route("/api/", methods=["POST"])
@@ -40,20 +28,21 @@ def receive_hakk():
 	begins_with = form["begins_with"]
 	ends_with = form["ends_with"]
 
-	this_many_words_as_string = form["how_many_words"]
 	try:
-		this_many_words = int(this_many_words_as_string)
+		this_many_words = int(form["how_many_words"])
 	except:
 		#Display that not a valid number
 		placeholder = None
 		this_many_words = None
+
 	if this_many_words:
 		results_list = parse_search_entry(entry, 'Synonym', begins_with, ends_with, this_many_words)
 	else:
 		results_list = parse_search_entry(entry, 'Synonym', begins_with, ends_with)
-	#if results_list is None:
-		#Display NO_RESULTS
-	results = ', '.join(results_list)
+	if results_list is None:
+		results = None
+	else:
+		results = ', '.join(results_list)
 
 	show = [entry, results]
 	return render_template('home.html', results = show)
