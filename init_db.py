2a547cc1987119646c760cbe27aaebe90d8d8fa5
Christopher Qian <cqian19@hotmail.com>
2015-11-10 16:59:36 -0800

    Removed files

--- init_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('hakks.db')
-c = conn.cursor()
-c.execute("CREATE TABLE hakks (name, datetime, hakk)")
-c.execute("INSERT INTO hakks VALUES ('bob', '100', 'Hello world!')")
-c.execute("SELECT * FROM hakks")
-print(c.fetchall())
-conn.commit()
-conn.close()
